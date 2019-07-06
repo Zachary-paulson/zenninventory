9c088afc0ef6679d759be8097bdbc35b46f43dfd
Zachary Paulson <zachary.paulson@outlook.com>
2019-07-06 16:58:59 -0700

    add store front cards and props

--- client/src/components/StoreFrontCard/StoreFrontCard.js
+++ client/src/components/StoreFrontCard/StoreFrontCard.js
@@ -2,34 +2,24 @@ import React, {Component} from "react";
 import "./style.css";
 
 class StoreFrontCard extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
-    return (
-      <div classNameName='continer-fuild'>
-        <div className="card StoreFrontCard" >
-          <div className="card-body">
-            <h5 className="card-title">Ebay Store front</h5>
-            <p className="card-text">Connect your Ebay Store Front with Zenn Inventory </p>
-            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-ebay"></i></a>
-          </div>
-        </div>
-        <div className="card StoreFrontCard" >
-          <div className="card-body">
-            <h5 className="card-title">Amazon Store front</h5>
-            <p className="card-text">Connect your Amazon Store Front with Zenn Inventory </p>
-            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-amazon"></i></a>
-          </div>
-        </div>
-        <div className="card StoreFrontCard" >
-          <div className="card-body">
-            <h5 className="card-title">Etsy Store front</h5>
-            <p className="card-text">Connect your Etsy Store Front with Zenn Inventory </p>
-            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-etsy"></i></a>
-          </div>
+    let storeIcon = `fab fa-${this.props.storeFront.toLocaleLowerCase()}`
+
+    return (<div className='continer-fuild'>
+      <div className="card StoreFrontCard">
+        <div className="card-body">
+          <h5 className="card-title">{this.props.storeFront} Store front</h5>
+          <p className="card-text">Connect your {this.props.storeFront} Store Front with Zenn Inventory
+          </p>
+          <a href="#" className="btn btn-primary">Connect to
+            <i className={storeIcon}></i>
+          </a>
         </div>
       </div>
-
-
-        );
-        }
-        }
-        export default StoreFrontCard;
+    </div>);
+  }
+}
+export default StoreFrontCard;
--- client/src/pages/Channels.js
+++ client/src/pages/Channels.js
@@ -1,69 +1,58 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 // import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
-import Summary from "../components/Summary";
 // import Wrapper from "../components/Wrapper";
 import Nav from "../components/Nav";
 import NavSide from "../components/NavSide";
 import API from "../utils/API";
 // import { Link } from "react-router-dom";
-import { Col } from "../components/Grid";
+import {Col} from "../components/Grid";
 // import { List, ListItem } from "../components/List";
 import StoreFrontCard from '../components/StoreFrontCard';
 
 class Channels extends Component {
-  state = {
-    items: [],
-    title: "",
-    author: "",
-    synopsis: "",
-    search: "",
-    results: []
-  };
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    // this.loadItems();
   }
-
-  loadItems = () => {
-    API.getItems()
-      .then(res =>
-        this.setState({ items: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
+  static defaultProps = {
+    store: [
+      {
+        name: 'Amazon'
+      },
+      {
+        name: 'Ebay'
+      },
+      {
+        name: 'Etsy'
+      },
+    ]
   };
-
 
   render() {
-    return (
-      <div>
-        {/* <Container fluid> */}
-        <Nav>
-        </Nav>
-        <Col size="md-12 sm-12">
-          <div className="row">
-            <Col size="md-2">
-              <NavSide>
-              </NavSide>
-            </Col>
-            <Col size="md-10">
-              <Jumbotron>
-                <h1>Storefront Channels</h1>
-                <div>Set up Amazon/Etsy/Ebay/etc</div>
-              </Jumbotron>
+    return (<div>
+      {/* <Container fluid> */}
+      <Nav />
+      <Col size="md-12 sm-12">
+        <div className="row">
+          <Col size="md-2">
+            <NavSide />
+          </Col>
+          <Col size="md-10">
+            <div className="row">
+              <Col size="md-12 sm-12">
+                {this.props.store.map((store) => (
+                  <StoreFrontCard storeFront={store.name}/>
+                ))}
 
-              <div className="row">
-                <Col size="md-12 sm-12">
-                  <StoreFrontCard/>
 
-                </Col>
-              </div>
-            </Col>
-          </div>
-        </Col>
+              </Col>
+            </div>
+          </Col>
+        </div>
+      </Col>
       {/* </Container > */}
-      </div>
-    );
+    </div>);
   }
 }
 
