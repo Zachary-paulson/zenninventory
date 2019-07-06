dded969aa1bcf8e7f816f784777efc5c298b4937
Zachary Paulson <zachary.paulson@outlook.com>
2019-07-06 09:20:55 -0700

    Added amazon and etsy cards

--- client/src/components/StoreFrontCard/StoreFrontCard.js
+++ client/src/components/StoreFrontCard/StoreFrontCard.js
@@ -6,11 +6,24 @@ class StoreFrontCard extends Component {
     return (
       <div classNameName='continer-fuild'>
         <div className="card StoreFrontCard" >
-          <img className="card-img-top" src="..." alt="Card image cap"/>
           <div className="card-body">
             <h5 className="card-title">Ebay Store front</h5>
             <p className="card-text">Connect your Ebay Store Front with Zenn Inventory </p>
-            <a href="#" className="btn btn-secondary">Sign in with  <i class="fab fa-ebay"></i></a>
+            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-ebay"></i></a>
+          </div>
+        </div>
+        <div className="card StoreFrontCard" >
+          <div className="card-body">
+            <h5 className="card-title">Amazon Store front</h5>
+            <p className="card-text">Connect your Amazon Store Front with Zenn Inventory </p>
+            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-amazon"></i></a>
+          </div>
+        </div>
+        <div className="card StoreFrontCard" >
+          <div className="card-body">
+            <h5 className="card-title">Etsy Store front</h5>
+            <p className="card-text">Connect your Etsy Store Front with Zenn Inventory </p>
+            <a href="#" className="btn btn-primary">Connect to <i class="fab fa-etsy"></i></a>
           </div>
         </div>
       </div>
