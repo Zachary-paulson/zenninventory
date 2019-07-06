6807c05573c4e3ca85620268ab73c64b5b582996
Zachary Paulson <zachary.paulson@outlook.com>
2019-07-06 08:48:27 -0700

    Connect font awesome and added store front card

--- client/src/components/StoreFrontCard/StoreFrontCard.js
+++ client/src/components/StoreFrontCard/StoreFrontCard.js
@@ -9,8 +9,8 @@ class StoreFrontCard extends Component {
           <img className="card-img-top" src="..." alt="Card image cap"/>
           <div className="card-body">
             <h5 className="card-title">Ebay Store front</h5>
-            <p className="card-text">Connect your Ebay store front with Zenn Inventory <i class="fab fa-ebay"></i></p>
-            <a href="#" className="btn btn-secondary">Sign in with  </a>
+            <p className="card-text">Connect your Ebay Store Front with Zenn Inventory </p>
+            <a href="#" className="btn btn-secondary">Sign in with  <i class="fab fa-ebay"></i></a>
           </div>
         </div>
       </div>
