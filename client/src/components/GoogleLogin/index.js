75322934e8b78d1c6bb2a10cf62a498dc1b746fb
Zachary Paulson <zachary.paulson@outlook.com>
2019-07-11 15:11:09 -0700

    fixing heroku

--- client/src/components/GoogleLogin/index.js
+++ client/src/components/GoogleLogin/index.js
@@ -2,18 +2,17 @@ import React, { Component } from "react";
 import "./style.css";
 
 class GoogleLogin extends Component {
-
-
   render() {
-    let googleAuthRoute = (process.env.NODE_ENV == 'production' ? `/auth/google`: `http://localhost:3001/auth/google`);
+    let googleAuthRoute = "/auth/google";
     return (
-
-<div className="form-group">
-  <a className="btn btn-warning btn-block my-1" href={googleAuthRoute}>
-    <span> <i className="fab fa-google"></i> Sign in with Google</span>
-  </a>
-</div>
-
+      <div className="form-group">
+        <a className="btn btn-warning btn-block my-1" href={googleAuthRoute}>
+          <span>
+            {" "}
+            <i className="fab fa-google" /> Sign in with Google
+          </span>
+        </a>
+      </div>
     );
   }
 }
